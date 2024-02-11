6ed4cfe6b4c33eb6d5f7a439b6b0f0f4aa82ce8f
Michael Ionov <imichaelionov@gmail.com>
2024-02-11 21:06:05 +0200

    something works

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import dayjs from 'dayjs';
 import _ from 'lodash';
 
 const MIN_REST = 8 * 60;
-const END_DAYS = 2;
+const END_DAYS = 4;
 const TIME_FORMAT = 'hh:mm';
 const MAX_SHIFT_TIME = 7200;
-const CONSIDERED_LAST_SHIFTS = 8;
+const CONSIDERED_LAST_SHIFTS = 24;
 
-const WEIGHT_REST = 100;
-const WEIGHT_SAME_PERSON = -400;
-const WEIGHT_SAME_HOUR = -200;
+const WEIGHT_REST = 1;
+const WEIGHT_SAME_PERSON = -5;
+const WEIGHT_SAME_HOUR = -4;
 
 const setHour = (date, h) => date.set('h', h).set('m', 0).set('s', 0).set('ms', 0);
 
@@ -43,7 +43,7 @@ const print = (shifts) => {
 };
 
 const validShift = (shifts, onDuty) => {
-  const lastShifts = _.take(_.reverse(_.cloneDeep(shifts)), CONSIDERED_LAST_SHIFTS);
+  const lastShifts = _.take(_.reverse(shifts), CONSIDERED_LAST_SHIFTS);
   if (!lastShifts.length) {
     return true;
   }
@@ -53,7 +53,7 @@ const validShift = (shifts, onDuty) => {
 };
 
 const getCost = ({ person, shifts, startTime, minPeople, onDuty }) => {
-  const lastShifts = _.filter(_.reverse(_.cloneDeep(shifts)), (shift) => {
+  const lastShifts = _.filter(_.take(_.reverse(_.cloneDeep(shifts)), CONSIDERED_LAST_SHIFTS), (shift) => {
     return !!shift.onDuty.includes(person);
   });
   if (onDuty.includes(person)) return -100000;
@@ -61,55 +61,26 @@ const getCost = ({ person, shifts, startTime, minPeople, onDuty }) => {
   const shiftTime = 4;
   const restPeriod = startTime.diff(lastShifts[0].startTime, 'h') - shiftTime; // heighest weight
   const sameShiftHour = startTime.get('h') === lastShifts[0].startTime.get('h');
-  console.log(lastShifts);
   const sameShiftPersons = onDuty.length < minPeople ? lastShifts.find((s) => s.onDuty.includes(onDuty[0])) : false;
   // const totalRestTime = 0 // TODO v2: sum time between shifts
 
   const restCost = restPeriod * WEIGHT_REST;
   const sameHourCost = sameShiftHour ? WEIGHT_SAME_HOUR : 0;
   const samePersonCost = sameShiftPersons ? WEIGHT_SAME_PERSON : 0;
-  if (onDuty.length < minPeople) {
-    console.log('person ', person, restCost + sameHourCost + samePersonCost);
-    console.log({
-      onDuty,
-      sameShiftPersons,
-      samePersonCost,
-      sameHourCost,
-      sameShiftHour,
-      restCost,
-      restPeriod,
-    });
-  }
 
   return restCost + sameHourCost + samePersonCost;
 };
 
-const getNextShift = ({ startTime, queue, shifts, minPeople, onDuty }) => {
+const populateOnDuty = ({ startTime, queue, shifts, minPeople, onDuty }) => {
   while (onDuty.length < minPeople) {
     const q = queue.map((person) => {
       const cost = getCost({ startTime, person, shifts, minPeople, onDuty });
       return { person, cost };
     });
     queue = _.orderBy(q, ['cost'], ['desc']).map((p) => p.person);
-    console.log('queue', startTime.format(TIME_FORMAT), queue);
-    onDuty.push(..._.take(queue, 1));
+    onDuty.push(_.first(queue));
   }
   // const v = validShift(shifts, onDuty);
-  // if (v) {
-  //   onDuty.forEach((p) => {
-  //     _.pull(queue, p);
-  //   });
-  //   queue.push(...onDuty);
-  // } else {
-  //   // while (!validShift(shifts, onDuty)) {
-  //   onDuty = _.shuffle(_.take(queue, minPeople * 2));
-  //   onDuty = _.sampleSize(onDuty, minPeople);
-  //   // console.log({ onDuty });
-  //   onDuty.forEach((p) => {
-  //     _.pull(queue, p);
-  //   });
-  //   queue.push(...onDuty);
-  // }
 };
 
 const generate = ({ stations, input, constraints, people }) => {
@@ -150,12 +121,8 @@ const generate = ({ stations, input, constraints, people }) => {
   const allShifts = [];
   shifts.forEach(({ minPeople, ...shift }) => {
     const onDuty = [];
-    getNextShift({ startTime, queue, shifts: allShifts, minPeople, onDuty });
-    allShifts.push({
-      ...shift,
-      minPeople,
-      onDuty,
-    });
+    populateOnDuty({ startTime, queue, shifts: allShifts, minPeople, onDuty });
+    allShifts.push({ ...shift, onDuty });
   });
 
   console.table(allShifts);
